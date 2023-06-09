ffb24971ff04d5a098a791b3cbdfa5fc2f143119
j3rryl <jeremymunroe0@gmail.com>
2023-06-09 19:53:22 +0300

    dictionary api

--- src/main/java/com/example/dictionary/Controller/ApiController.java
+++ src/main/java/com/example/dictionary/Controller/ApiController.java
@@ -4,12 +4,16 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.dictionary.Models.User;
+import com.example.dictionary.Models.Words;
 import com.example.dictionary.Repos.UserRepo;
+import com.example.dictionary.Repos.WordsRepo;
 
 @RestController
 public class ApiController {
@@ -17,6 +21,10 @@ public class ApiController {
     //Handles all dependency dependencies
     @Autowired
     private UserRepo userRepo;
+
+    @Autowired
+    private WordsRepo wordsRepo;
+    
     @GetMapping(value="/")
     public String getPage(){
         return "Welcome";
@@ -32,5 +40,24 @@ public class ApiController {
         //Because it is json hence te @RequestBody
         userRepo.save(user);
         return "Saved";
+    }
+
+    @PutMapping(value = "/update/{id}")
+    public String updateUser(@PathVariable long id, @RequestBody User user){
+        //Because it is json hence te @RequestBody
+        User updatedUser = userRepo.findById(id).get();
+        updatedUser.setFirstName(user.getFirstName());
+        updatedUser.setLastName(user.getLastName());
+        updatedUser.setOccupation(user.getOccupation());
+        updatedUser.setAge(user.getAge());
+
+        userRepo.save(updatedUser);
+        return "User Updated";
+    }
+
+    //Dictionary
+    @GetMapping(value = "/dictionary")
+    public List<Words> getDictionary(){
+        return wordsRepo.findAll();
     }
 }
--- src/main/java/com/example/dictionary/Models/Student.java
+++ src/main/java/com/example/dictionary/Models/Student.java
@@ -1,4 +1,4 @@
-package com.example.dictionary;
+package com.example.dictionary.Models;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
